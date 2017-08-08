ab4b1054d47a62cc27d5ef78078162ee71059f43
celsinnn <irmaocelsinho@gmail.com>
2017-08-08 20:37:29 -0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -252,8 +252,8 @@ function showMessage(msg){
 /* Valores padrão 
  */
 var defaultValues = {
-	urlServidor : 'http://127.0.0.1/conferencias',
-	// urlServidor : 'http://inscricaoconferenciabh.000webhostapp.com',
+	// urlServidor : 'http://127.0.0.1/conferencias',
+	urlServidor : 'http://inscricaoconferenciabh.000webhostapp.com',
 	// urlServidor : 'http://www.treinasusfacil.mg.gov.br/acompanhamento/relatorio/CRE/CNES_IMP/listagfcms.json',
 	timeoutDefault : 10000,
 	tryCountDefault : 0,
